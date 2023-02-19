d78ef9d9d1692d68e4ec902602fcf776f9e42a96
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-02-19 12:20:31 -0500

    backend changes to api endpoint for completion

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,25 +1,39 @@
-//Router File for handling all the requests
-
 const express = require('express');
-const axios = require('axios');
 const router = express.Router();
+const keySetUp = require('../private/key');
+const openAiKey = keySetUp.myKey;
+const { Configuration, OpenAIApi } = require('openai');
 
-//API Keys
-const keyImport = require('../configs/myKey'); // require the key from the configs folder
-const apiKey = keyImport.apiKey; // assign the key to a variable
-//End of API Keys
+const configuration = new Configuration({
+  apiKey: openAiKey,
+});
 
-//OPEN AI SETUP
-const { OpenAIApi } = require('openai');
-const openai_api = new OpenAIApi(apiKey);
-//END OF OPEN AI SETUP
+const openAi = new OpenAIApi(configuration);
 
-//ROUTES
+router.post('/completion', async (req, res) => {
+  const prompt = req.body.prompt;
+  const bestOf = req.body.bestOf;
+  const frequencyPenalty = req.body.frequencyPenalty;
+  const maxTokens = req.body.maxTokens;
+  const presencePenalty = req.body.presencePenalty;
+  const topP = req.body.topP;
+
+  const openAiResponse = await openAi.createCompletion({
+    model: 'davinci',
+    prompt: prompt,
+    temperature: 0.9,
+    maxTokens: maxTokens,
+    topP: topP,
+    presencePenalty: presencePenalty,
+    frequencyPenalty: frequencyPenalty,
+    bestOf: bestOf,
+  });
+
+  res.send(openAiResponse.data);
+});
+
 router.get('/', (req, res) => {
   res.send('Welcome to the backend of the application');
 });
 
-router.post('/answers', async (req, res) => {});
-
-//End of ROUTES
 module.exports = router;
